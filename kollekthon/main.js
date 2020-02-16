da836b002ba10d790050bc9fe6e5c62cadf7563d
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2020-02-16 08:16:45 -0400

    update

---
+++ kollekthon/main.js
@@ -0,0 +1,26 @@
+import { CountUp } from './countUp.min.js';
+
+window.onload = function() {
+  var xmlhttp = new XMLHttpRequest();
+  var url = "total.json";
+
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          newval = JSON.parse(this.responseText).total;
+
+          // newval = currentval + 1123;
+          var countUp = new CountUp('total', newval, { startVal: currentval, duration: 2, separator:' ', suffix: " €" });
+          currentval = newval;
+          countUp.start();
+      }
+  };
+  function updatetotal() {
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+  }
+
+  window.setInterval(function() {
+    updatetotal();
+  }, 10000);
+  updatetotal();
+}
